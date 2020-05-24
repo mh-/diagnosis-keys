220371c8a4432ea17fd167083a4fab9fc02ebc01
Michael Huebler <michael@huebler.org>
2020-05-24 10:01:18 +0200

    Initial commit.

---
+++ lib/conversions.py
@@ -0,0 +1,20 @@
+import datetime
+
+interval_length_minutes = 10  # 10 minutes per interval
+tek_rolling_period = 144  # 24*60//10 - 24 hours per day, 60 minutes per hour, 10 minutes per interval
+
+
+def get_timestamp_from_interval(interval_number):
+    return interval_number * interval_length_minutes * 60  # 60 seconds per minute
+
+
+def get_datetime_from_utc_timestamp(utc_timestamp):
+    return datetime.datetime.utcfromtimestamp(utc_timestamp).replace(tzinfo=datetime.timezone.utc)
+
+
+def get_local_datetime(date_time):
+    return date_time.astimezone(datetime.datetime.utcnow().astimezone().tzinfo)
+
+
+def get_string_from_datetime(date_time):
+    return date_time.strftime('%Y-%m-%d %H:%M:%S %Z')
---
+++ parse_keys.py
@@ -0,0 +1,49 @@
+from lib.conversions import *
+import TemporaryExposureKeyExport_pb2
+from zipfile import ZipFile
+import datetime
+
+file_name = "testExport-2-records-1-of-1.zip"
+# file_name = "testExport-450-records-1-of-5.zip"
+
+# The binary format file consists of a 16 byte header, containing "EK Export v1​" right padded with whitespaces in UTF-8
+label = "EK Export v1"
+header_str = label + ' ' * (16-len(label))
+header = header_str.encode("UTF-8")
+
+zip_file = ZipFile(file_name)
+export_bin = zip_file.read("export.bin")
+
+if not export_bin[:len(header)] == header:
+    print("ERROR: export.bin (extracted from %s) does not start with '%s'" % (file_name, header_str))
+    exit(1)
+
+tek_keys_export = TemporaryExposureKeyExport_pb2.TemporaryExposureKeyExport()
+tek_keys_export.ParseFromString(export_bin[len(header):])
+
+# print(tek_keys_export)
+
+print("File '%s' read." % file_name)
+
+start_timestamp_utc = get_datetime_from_utc_timestamp(tek_keys_export.start_timestamp)
+end_timestamp_utc = get_datetime_from_utc_timestamp(tek_keys_export.end_timestamp)
+print("- Time window: %s - %s" % (get_string_from_datetime(get_local_datetime(start_timestamp_utc)),
+                                  get_string_from_datetime(get_local_datetime(end_timestamp_utc))))
+print("- Region: %s" % tek_keys_export.region)
+print("- Batch: %d of %d" % (tek_keys_export.batch_num, tek_keys_export.batch_size))
+
+for signature_info in tek_keys_export.signature_infos:
+    print("- Signature Info:")
+    print(signature_info)
+
+print("TEKs:")
+i = 0
+for tek in tek_keys_export.keys:
+    i += 1
+    print("%3d: TEK: %s, Transmission Risk Level: %d, Time: %s - %s (%d, %d)" %
+          (i, tek.key_data.hex(), tek.transmission_risk_level,
+           get_string_from_datetime(get_local_datetime(get_datetime_from_utc_timestamp(get_timestamp_from_interval(
+               tek.rolling_start_interval_number)))),
+           get_string_from_datetime(get_local_datetime(get_datetime_from_utc_timestamp(get_timestamp_from_interval(
+               tek.rolling_start_interval_number+tek.rolling_period)))),
+           tek.rolling_start_interval_number, tek.rolling_period))
