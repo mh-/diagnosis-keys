c794c8693be91c22f6f7063f8315fd8a05c986a2
Michael Huebler <michael@huebler.org>
2020-05-24 18:49:03 +0200

    Added matching with Scanned RPIs (-r rx_data.csv)

---
+++ lib/scanned_rpis.py
@@ -0,0 +1,49 @@
+import csv
+from lib.conversions import *
+
+
+class RPIEntry:
+    def __init__(self, start_time, end_time, rpi, aem, rssi, bdaddr, lat=None, lon=None, altitude=None, speed=None):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.rpi = rpi
+        self.aem = aem
+        self.rssi = rssi
+        self.bdaddr = bdaddr
+        self.lat = lat
+        self.lon = lon
+        self.altitude = altitude
+        self.speed = speed
+
+
+class ScannedRPIs:
+    def __init__(self, file_name):
+        self.rpis_dict = dict()
+        with open(file_name, newline='') as rpi_file:
+            rpi_reader = csv.reader(rpi_file, delimiter=';')
+            # StartTime, EndTime, RPI, AEM, MaxRSSI, BDADDR [, LAT, LON, Altitude, Speed]
+            for row in rpi_reader:
+                if row[0] == "StartTime":
+                    continue
+                if len(row) == 6:
+                    rpi = RPIEntry(get_datetime_from_utc_timestamp(int(row[0])),
+                                   get_datetime_from_utc_timestamp(int(row[1])),
+                                   bytes.fromhex(row[2]), bytes.fromhex(row[3]),
+                                   int(row[4]), row[5])
+                elif len(row) == 10:
+                    rpi = RPIEntry(get_datetime_from_utc_timestamp(int(row[0])),
+                                   get_datetime_from_utc_timestamp(int(row[1])),
+                                   bytes.fromhex(row[2]), bytes.fromhex(row[3]),
+                                   int(row[4]), row[5],
+                                   lat=row[6], lon=row[7], altitude=row[8], speed=row[9])
+                else:
+                    print("ERROR: %s must have only rows with 6 or 10 entries, found a row with %d entries" %
+                          (file_name, len(row)))
+                    exit(1)
+                if rpi.rpi in self.rpis_dict:
+                    existing_entries = self.rpis_dict[rpi.rpi]
+                    existing_entries.append(rpi)
+                    self.rpis_dict[rpi.rpi] = existing_entries
+                else:
+                    self.rpis_dict[rpi.rpi] = [rpi]
+        # print(self.rpis)
--- parse_keys.py
+++ parse_keys.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from lib.diagnosis_keys import *
+from lib.scanned_rpis import *
 from lib.conversions import *
 from lib.crypto import *
 import argparse
@@ -8,16 +9,25 @@ import argparse
 parser = argparse.ArgumentParser(description="Exposure Notification Diagnosis Key Parser.")
 parser.add_argument("-d", "--diagnosiskeys", type=str, default="testExport-2-records-1-of-1.zip",
                     help="file name of the Diagnosis Keys .zip file")
+parser.add_argument("-r", "--rpis", type=str, default="",
+                    help="file name of the Scanned RPIs .csv file")
 args = parser.parse_args()
 
-file_name = args.diagnosiskeys
+dk_file_name = args.diagnosiskeys
+rpi_file_name = args.rpis
+read_rpi_file = (rpi_file_name != "")
 
 print("Exposure Notification Diagnosis Key Parser")
 print("This script parses published Diagnosis Keys.\n")
 # see https://github.com/google/exposure-notifications-server/tree/master/examples/export
 
-dk = DiagnosisKeys(file_name)
-print("File '%s' read." % file_name)
+dk = DiagnosisKeys(dk_file_name)
+print("File '%s' read." % dk_file_name)
+
+scanned_rpis = None
+if read_rpi_file:
+    scanned_rpis = ScannedRPIs(rpi_file_name)
+    print("File '%s' read." % rpi_file_name)
 
 start_timestamp_utc = dk.get_upload_start_timestamp()
 end_timestamp_utc = dk.get_upload_end_timestamp()
@@ -41,7 +51,29 @@ for tek in dk.get_keys():
            get_string_from_datetime(get_local_datetime(get_datetime_from_utc_timestamp(get_timestamp_from_interval(
                tek.rolling_start_interval_number+tek.rolling_period)))),
            tek.rolling_start_interval_number, tek.rolling_period))
-    print("     RPIs: ", end="")
-    for rpi in get_rpis(derive_rpi_key(tek.key_data), tek.rolling_start_interval_number, tek.rolling_period):
-        print("%s " % rpi.hex(), end="")
-    print()
+
+    if read_rpi_file:
+        for diagnosis_rpi in get_rpis(derive_rpi_key(tek.key_data), tek.rolling_start_interval_number, tek.rolling_period):
+            # print(diagnosis_rpi, end='')
+            if diagnosis_rpi in scanned_rpis.rpis_dict:
+                print("FOUND MATCH!")
+                aem_key = derive_aem_key(tek.key_data)
+                scanned_rpi_entries = scanned_rpis.rpis_dict[diagnosis_rpi]
+                for scanned_rpi_entry in scanned_rpi_entries:
+                    metadata = decrypt_aem(aem_key, scanned_rpi_entry.aem, diagnosis_rpi)
+                    print("Timeframe: %s - %s (%d seconds), RSSI: %d dBm, LAT, LON, altitude, speed: %s, %s, %s, %s" %
+                          (get_string_from_datetime(get_local_datetime(scanned_rpi_entry.start_time)),
+                           get_string_from_datetime(get_local_datetime(scanned_rpi_entry.end_time)),
+                           (scanned_rpi_entry.end_time-scanned_rpi_entry.start_time).total_seconds(),
+                           scanned_rpi_entry.rssi,
+                           scanned_rpi_entry.lat, scanned_rpi_entry.lon,
+                           scanned_rpi_entry.altitude, scanned_rpi_entry.speed))
+                    print("Decrypted metadata: %s " % metadata.hex(), end='')
+                    if metadata[0] == 0x40:
+                        tx_power = struct.unpack_from("b", metadata, 1)[0]  # signed char
+                        print("--> TX Power: %d dBm --> RF attenuation: %d dB" %
+                              (tx_power, tx_power-scanned_rpi_entry.rssi), end='')
+                    else:
+                        print("--> WARNING: Expected metadata to start with 40, so this could be a false positive!")
+                    print()
+        # print()
