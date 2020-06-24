59bd242ad8ea9dfd44aba9964489280c20cabcb3
Michael Huebler <michael@huebler.org>
2020-06-24 12:09:35 +0200

    Account for Diagnosis Key files with 2 submission dates

--- lib/count_users.py
+++ lib/count_users.py
@@ -6,60 +6,65 @@ def count_users(diagnosis_key_list):
     print("Length: %d keys (%d without padding)" % (len(diagnosis_key_list), len(diagnosis_key_list) // 10))
     if len(diagnosis_key_list) % 10 != 0:
         print("WARNING: Apparently the list is not padded by 10!")
-    latest_interval = max(dk.start_interval for dk in diagnosis_key_list)
 
     user_days = []
-
-    # First search for 'invalid' TRL profiles, caused by old Android apps
-    # (before this fix was released: https://github.com/corona-warn-app/cwa-app-android/pull/679)
-
     num_old_android_apps = 0
-    top_level_dks = [dk for dk in diagnosis_key_list if dk.start_interval == latest_interval]
-    for dk in top_level_dks:
-        try:
-            pos = trl_profile[2:].index(dk.transmission_risk_level)+2
-        except ValueError:
-            continue
-        diagnosis_key_list.remove(dk)
-        days = 1
-        interval = latest_interval
-        while pos > 1:
-            interval -= 144
-            next_dk = next((dk_entry for dk_entry in diagnosis_key_list
-                            if (dk_entry.start_interval == interval) and
-                            (dk_entry.transmission_risk_level == trl_profile[pos-1])), None)
-            if (next_dk is None) and (trl_profile[pos] == 8):
-                pos += 1
-                next_dk = next((dk_entry for dk_entry in diagnosis_key_list
-                                if (dk_entry.start_interval == interval) and
-                                (dk_entry.transmission_risk_level == trl_profile[pos - 1])), None)
-            if next_dk is None:
-                break
-            diagnosis_key_list.remove(next_dk)
-            days += 1
-            pos -= 1
-        user_days.append(days)
-        num_old_android_apps += 1
 
-    # Now search for 'standard' TRL profiles
+    for i in [0, 1]:
+        # do this twice, because if keys are distributed in the morning,
+        # they could include yesterday's submissions
 
-    top_level_dks = [dk for dk in diagnosis_key_list if dk.start_interval == latest_interval]
-    for dk in top_level_dks:
-        days = 0
-        if dk.transmission_risk_level == trl_profile[1]:  # newest entry is from yesterday
+        # First search for 'invalid' TRL profiles, caused by old Android apps
+        # (before this fix was released: https://github.com/corona-warn-app/cwa-app-android/pull/679)
+
+        latest_interval = max(dk.start_interval for dk in diagnosis_key_list)
+        top_level_dks = [dk for dk in diagnosis_key_list if dk.start_interval == latest_interval]
+        for dk in top_level_dks:
+            try:
+                pos = trl_profile[2:].index(dk.transmission_risk_level)+2
+            except ValueError:
+                continue
             diagnosis_key_list.remove(dk)
             days = 1
             interval = latest_interval
-            while days < len(trl_profile):
+            while pos > 1:
                 interval -= 144
                 next_dk = next((dk_entry for dk_entry in diagnosis_key_list
                                 if (dk_entry.start_interval == interval) and
-                                (dk_entry.transmission_risk_level == trl_profile[days+1])), None)
+                                (dk_entry.transmission_risk_level == trl_profile[pos-1])), None)
+                if (next_dk is None) and (trl_profile[pos] == 8):
+                    pos += 1
+                    next_dk = next((dk_entry for dk_entry in diagnosis_key_list
+                                    if (dk_entry.start_interval == interval) and
+                                    (dk_entry.transmission_risk_level == trl_profile[pos - 1])), None)
                 if next_dk is None:
                     break
                 diagnosis_key_list.remove(next_dk)
                 days += 1
-        user_days.append(days)
+                pos -= 1
+            user_days.append(days)
+            num_old_android_apps += 1
+
+        # Now search for 'standard' TRL profiles
+
+        latest_interval = max(dk.start_interval for dk in diagnosis_key_list)
+        top_level_dks = [dk for dk in diagnosis_key_list if dk.start_interval == latest_interval]
+        for dk in top_level_dks:
+            days = 0
+            if dk.transmission_risk_level == trl_profile[1]:  # newest entry is from yesterday
+                diagnosis_key_list.remove(dk)
+                days = 1
+                interval = latest_interval
+                while days < len(trl_profile):
+                    interval -= 144
+                    next_dk = next((dk_entry for dk_entry in diagnosis_key_list
+                                    if (dk_entry.start_interval == interval) and
+                                    (dk_entry.transmission_risk_level == trl_profile[days+1])), None)
+                    if next_dk is None:
+                        break
+                    diagnosis_key_list.remove(next_dk)
+                    days += 1
+            user_days.append(days)
 
     reduced_users = len(user_days) // 10
     print("%d user(s) found." % reduced_users)
