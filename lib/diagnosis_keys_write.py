37e297baba9d437971199246c1e1539a4cf81afe
Michael Huebler <michael@huebler.org>
2020-07-29 18:21:39 +0200

    Modification for more random demo data

--- lib/diagnosis_keys_write.py
+++ lib/diagnosis_keys_write.py
@@ -1,3 +1,5 @@
+import random
+
 import TemporaryExposureKeyExportV1_5_pb2
 import zipfile
 import secrets
@@ -6,7 +8,8 @@ from lib.conversions import *
 TRL_PROFILE = [5, 6, 8, 8, 8, 5, 3, 1, 1, 1, 1, 1, 1, 1]  # 0: today, 1: yesterday, ...
 
 NUMBER_OF_UPLOAD_DAYS = 14
-NUMBER_OF_UPLOADS_PER_DAY = 20
+NUMBER_OF_UPLOADS_PER_DAY_MIN = 5
+NUMBER_OF_UPLOADS_PER_DAY_MAX = 20
 ENIN_ONE_DAY = 144
 LAST_DAY = "2020-05-23-+0000"
 
@@ -19,11 +22,13 @@ class DiagnosisKeysWriter:
         last_day = datetime.datetime.strptime(LAST_DAY, "%Y-%m-%d-%z")
         last_enin = get_interval_from_utc_timestamp(get_timestamp_from_utc_datetime(last_day))
         for upload_enin in range(last_enin, last_enin-int(NUMBER_OF_UPLOAD_DAYS*ENIN_ONE_DAY), -ENIN_ONE_DAY):
-            for _ in range(NUMBER_OF_UPLOADS_PER_DAY):
+            number_of_uploads_per_day = random.randint(NUMBER_OF_UPLOADS_PER_DAY_MIN, NUMBER_OF_UPLOADS_PER_DAY_MAX)
+            for _ in range(number_of_uploads_per_day):
                 for pos in range(13):
                     key_data = secrets.token_bytes(16)
+                    # key_data = bytes.fromhex("a45f81ba2b2c533edbe87a3db47733c4")
                     trl = TRL_PROFILE[pos+1]
-                    start_enin = upload_enin - pos*ENIN_ONE_DAY
+                    start_enin = upload_enin - (pos+1)*ENIN_ONE_DAY
                     duration = ENIN_ONE_DAY
                     report_type = TemporaryExposureKeyExportV1_5_pb2.TemporaryExposureKey.ReportType.CONFIRMED_TEST
                     self.add_key(key_data, trl, start_enin, duration, report_type)
